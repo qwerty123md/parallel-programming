1785d4d4759a712c7b3d56cc032bb8ca16cbb3c8
qwerty123 <ilyamateyuk@gmail.com>
2020-02-28 13:21:05 +0700

    release

---
+++ lab_2/cmake-build-release/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/qwerty123/parallel-programming/lab_1
+# Build directory: /home/qwerty123/parallel-programming/lab_1/cmake-build-release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(testlab_1 "/home/qwerty123/parallel-programming/lab_1/test/testlab_1" "./lab_1")
+set_tests_properties(testlab_1 PROPERTIES  DEPENDS "lab_1" _BACKTRACE_TRIPLES "/home/qwerty123/parallel-programming/modules/test_runner.cmake;23;add_test;/home/qwerty123/parallel-programming/modules/test_runner.cmake;0;;/home/qwerty123/parallel-programming/modules/common_lab.cmake;3;include;/home/qwerty123/parallel-programming/modules/common_lab.cmake;0;;/home/qwerty123/parallel-programming/lab_1/CMakeLists.txt;13;include;/home/qwerty123/parallel-programming/lab_1/CMakeLists.txt;0;")
---
+++ lab_2/src/cgm.c
@@ -0,0 +1,139 @@
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+#include "cgm.h"
+
+timers_t timers;
+static double start, end;
+
+static void add_vectors(const double *a, const double *b, size_t size, double *res) {
+	start = omp_get_wtime(); 
+	#pragma omp for
+	for (size_t i = 0; i < size; ++i) {
+		res[i] = a[i] + b[i];
+	}
+
+	end = omp_get_wtime(); 
+	timers.add_vect += end - start;
+}
+
+static void sub_vectors(const double *a, const double *b, size_t size, double *res) {
+	#pragma omp for
+	for (size_t i = 0; i < size; ++i) {
+		res[i] = a[i] - b[i];
+	}
+}
+
+static void mul_matrix_vector(const double *mat, const double *vec, size_t size, double *res) {
+	start = omp_get_wtime(); 
+	
+	#pragma omp for schedule(static) //static dynamic guided
+	for (size_t i = 0; i < size; ++i) {
+		double row_sum = 0.0;
+		for (size_t j = 0; j < size; ++j) {
+			row_sum += mat[i * size + j] * vec[j];
+		}
+		res[i] = row_sum;
+	}
+	
+	end = omp_get_wtime(); 
+	timers.mul_mat_vec += end - start;
+}
+static void mul_num_vector(double a, double *vec, size_t size, double *res) {
+	start = omp_get_wtime(); ;
+
+	#pragma omp for
+	for (size_t i = 0; i < size; ++i) {
+		res[i] = a * vec[i];
+	}
+
+end = omp_get_wtime(); 
+	timers.mul_num_vec += end - start;
+}
+
+static double scalar_mul(const double *a, const double *b, size_t size) {
+	start = omp_get_wtime(); 
+
+	double result = 0.0;
+	
+	for (size_t i = 0; i < size; ++i) {
+		result += a[i] * b[i];
+	}
+	
+end = omp_get_wtime(); 
+	timers.scalar_mul += end - start;
+	return result;
+}
+
+static bool check_end_alg(const double *r, const data_t *data) {
+start = omp_get_wtime(); 
+
+	double norm1 = 0.0;
+	double norm2 = 0.0;
+	
+	for (size_t i = 0; i < data->size; ++i) {
+		norm1 += r[i] * r[i];
+		norm2 += data->result_vector[i] * data->result_vector[i];
+	}
+	norm1 = sqrt(norm1);
+	norm2 = sqrt(norm2);
+
+	if (norm2 == 0.0)
+	{
+		perror("Division by zero in check_end_alg");
+		exit(EXIT_FAILURE);
+	}
+
+end = omp_get_wtime(); 
+	timers.check += end - start;
+	return (norm1 / norm2 < EPSILON) ? true : false;
+}
+
+int do_magic(data_t *data) {                                     //Conjugate Gradient Method
+	double a = 0.0;
+	double b = 0.0;
+	double scalar_prev_r = 0;
+
+	size_t size = data->size;
+
+	double *tmp = calloc(size, sizeof(double));
+	double *r = calloc(size, sizeof(double));
+	double *z = calloc(size, sizeof(double));
+
+	if (!(tmp && r && z)) {
+		perror("calloc err");
+		return EXIT_FAILURE;
+	}
+
+	mul_matrix_vector(data->coef_matrix, data->x_vector, size, tmp);
+	sub_vectors(data->result_vector, tmp, size, r);
+	memcpy(z, r, data->size * sizeof(double));
+
+	#pragma omp parallel shared(a, b, scalar_prev_r, tmp, r, z, data) 
+	{
+		while (!check_end_alg(r, data)) {
+		
+			mul_matrix_vector(data->coef_matrix, z, size, tmp);       //a
+			a = scalar_mul(r, r, size) / scalar_mul(tmp, z, size);
+
+			scalar_prev_r = scalar_mul(r, r, size);
+			#pragma omp barrier
+			mul_num_vector(a, tmp, size, tmp);                        //r
+			sub_vectors(r, tmp, size, r);
+			
+			mul_num_vector(a, z, size, tmp);                          //x
+			add_vectors(data->x_vector, tmp, size, data->x_vector);
+
+			b = scalar_mul(r, r, size) / scalar_prev_r;               //b
+
+			mul_num_vector(b, z, size, z);                            //z
+			add_vectors(r, z, size, z);
+			#pragma omp barrier
+		}
+	}
+
+	free(tmp);
+	free(r);
+	free(z);
+	return 0;
+}
