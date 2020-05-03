35114429f336810284a82a05891a65d45c17afaa
qwerty_ <ilyamateyuk@gmail.com>
2020-05-04 00:10:21 +0700

    lab_3

---
+++ lab_3/cmake-build-debug/CMakeFiles/lab_3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_3.dir/src/main.c.o"
+  "CMakeFiles/lab_3.dir/src/mul_grid.c.o"
+  "lab_3"
+  "lab_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab_3/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/qwerty123/parallel-programming/lab_3
+# Build directory: /home/qwerty123/parallel-programming/lab_3/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(testlab_3 "/home/qwerty123/parallel-programming/lab_3/test/testlab_3" "./lab_3")
+set_tests_properties(testlab_3 PROPERTIES  DEPENDS "lab_3" _BACKTRACE_TRIPLES "/home/qwerty123/parallel-programming/modules/test_runner.cmake;23;add_test;/home/qwerty123/parallel-programming/modules/test_runner.cmake;0;;/home/qwerty123/parallel-programming/modules/common_lab.cmake;3;include;/home/qwerty123/parallel-programming/modules/common_lab.cmake;0;;/home/qwerty123/parallel-programming/lab_3/CMakeLists.txt;13;include;/home/qwerty123/parallel-programming/lab_3/CMakeLists.txt;0;")
